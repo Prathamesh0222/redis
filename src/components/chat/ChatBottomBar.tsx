44c8e1442240f782b04d071e92298c8df4d18abb
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-08-14 21:53:19 +0530

    feat: Add route for signing Cloudinary params

--- src/components/chat/ChatBottomBar.tsx
+++ src/components/chat/ChatBottomBar.tsx
@@ -1,77 +1,135 @@
-import { AnimatePresence, motion } from "framer-motion"
-import { Image as ImageIcon, Loader, SendHorizonal, ThumbsUp } from "lucide-react"
-import { Textarea } from "../ui/textarea"
-import { useRef, useState } from "react"
-import EmojiPicker from "./EmojiPicker"
-import { Button } from "../ui/button"
-import useSound from "use-sound"
-import { usePreferences } from "@/store/usePreferences"
-import { useMutation } from "@tanstack/react-query"
-import { sendMessageAction } from "@/actions/message.actions"
-import { useSelectedUser } from "@/store/useSelectedUser"
+import { AnimatePresence, motion } from "framer-motion";
+import {
+  Image as ImageIcon,
+  Loader,
+  SendHorizonal,
+  ThumbsUp,
+} from "lucide-react";
+import { Textarea } from "../ui/textarea";
+import { useRef, useState } from "react";
+import EmojiPicker from "./EmojiPicker";
+import { Button } from "../ui/button";
+import useSound from "use-sound";
+import { usePreferences } from "@/store/usePreferences";
+import { useMutation } from "@tanstack/react-query";
+import { sendMessageAction } from "@/actions/message.actions";
+import { useSelectedUser } from "@/store/useSelectedUser";
+import { CldUploadWidget, CloudinaryUploadWidgetInfo } from "next-cloudinary";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
+import Image from "next/image";
 
 const ChatBottomBar = () => {
-  const [message, setMessage] = useState("")
-  const textAreaRef = useRef<HTMLTextAreaElement>(null)
-  const {selectedUser} = useSelectedUser();
-  const {soundEnabled} = usePreferences();
-  const [playSound1] = useSound("/sounds/keystroke1.mp3")
-  const [playSound2] = useSound("/sounds/keystroke2.mp3")
-  const [playSound3] = useSound("/sounds/keystroke3.mp3")
-  const [playSound4] = useSound("/sounds/keystroke4.mp3")
+  const [message, setMessage] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const { selectedUser } = useSelectedUser();
+  const { soundEnabled } = usePreferences();
+  const [imgUrl, setImgUrl] = useState("");
+  const [playSound1] = useSound("/sounds/keystroke1.mp3");
+  const [playSound2] = useSound("/sounds/keystroke2.mp3");
+  const [playSound3] = useSound("/sounds/keystroke3.mp3");
+  const [playSound4] = useSound("/sounds/keystroke4.mp3");
 
-  const playSoundFunctions = [playSound1, playSound2, playSound3, playSound4]
+  const playSoundFunctions = [playSound1, playSound2, playSound3, playSound4];
 
   const playRandomKeyStrokeSound = () => {
-    const randomIndex = Math.floor(Math.random() * playSoundFunctions.length)
-    soundEnabled && playSoundFunctions[randomIndex]()
-  }
+    const randomIndex = Math.floor(Math.random() * playSoundFunctions.length);
+    soundEnabled && playSoundFunctions[randomIndex]();
+  };
 
-  const {mutate:sendMessage, isPending} = useMutation({
-    mutationFn: sendMessageAction
-  })
+  const { mutate: sendMessage, isPending } = useMutation({
+    mutationFn: sendMessageAction,
+  });
 
   const handleSendMessage = () => {
-    if(!message.trim()) return;
+    if (!message.trim()) return;
 
     sendMessage({
       content: message,
       receiverId: selectedUser?.id!,
-      messageType: "text"
-    })
-    setMessage("")
-    textAreaRef.current?.focus()
+      messageType: "text",
+    });
+    setMessage("");
+    textAreaRef.current?.focus();
+  };
 
-  }
-
-  const handleEnterPress =(e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-      if(e.key === "Enter" && !e.shiftKey){
-        e.preventDefault();
-        handleSendMessage();
-      }
-      if(e.key ==="Enter" && e.shiftKey){
-        e.preventDefault();
-        setMessage(message + "\n")
-      }
-  }
+  const handleEnterPress = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSendMessage();
+    }
+    if (e.key === "Enter" && e.shiftKey) {
+      e.preventDefault();
+      setMessage(message + "\n");
+    }
+  };
 
   return (
-    <div className="p-2 flex justify-between
-     w-full items-center gap-2">
-      { !message.trim() && <ImageIcon size={20} className="cursor-pointer text-muted-foreground"/>}
+    <div
+      className="p-2 flex justify-between
+     w-full items-center gap-2"
+    >
+      {!message.trim() && (
+        <CldUploadWidget
+          signatureEndpoint={"/api/sign-cloudinary-params"}
+          onSuccess={(result, { widget }) => {
+            setImgUrl((result.info as CloudinaryUploadWidgetInfo).secure_url);
+            widget.close();
+          }}
+        >
+          {({ open }) => {
+            return (
+              <ImageIcon
+                size={20}
+                onClick={() => open()}
+                className="cursor-pointer text-muted-foreground"
+              />
+            );
+          }}
+        </CldUploadWidget>
+      )}
+
+      <Dialog open={!!imgUrl}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Image Preview</DialogTitle>
+          </DialogHeader>
+          <div className="flex justify-center items-center relative h-96 w-full mx-auto">
+            <Image
+              src={imgUrl}
+              alt="Image Preview"
+              fill
+              className="object-contain"
+            />
+          </div>
+          <DialogFooter>
+            <Button
+              type="submit"
+              onClick={() => {
+                sendMessage({
+                  content: imgUrl,
+                  messageType: "image",
+                  receiverId: selectedUser?.id!,
+                });
+                setImgUrl("");
+              }}
+            >
+              Send
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
       <AnimatePresence>
         <motion.div
-        layout
-        initial={{opacity:0, scale:1}}
-        animate={{opacity:1, scale:1}}
-        exit={{opacity:0, scale:1}}
-        transition={{
-          opacity: {duration:0.5},
-          layout: {type:"spring",
-          bounce:0.15,
-          }
-        }}
-        className="w-full relative"
+          layout
+          initial={{ opacity: 0, scale: 1 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 1 }}
+          transition={{
+            opacity: { duration: 0.5 },
+            layout: { type: "spring", bounce: 0.15 },
+          }}
+          className="w-full relative"
         >
           <Textarea
             autoComplete="off"
@@ -82,49 +140,59 @@ const ChatBottomBar = () => {
             value={message}
             ref={textAreaRef}
             onKeyDown={handleEnterPress}
-            onChange={(e)=>{
-              setMessage(e.target.value)
-              playRandomKeyStrokeSound()
+            onChange={(e) => {
+              setMessage(e.target.value);
+              playRandomKeyStrokeSound();
             }}
           />
           <div className="absolute right-2 bottom-0.5">
-          <EmojiPicker
-          onChange={(emoji)=>{
-            if(textAreaRef.current){
-              textAreaRef.current.focus()
-            }
-            setMessage(message + emoji)
-          }}
-          />
+            <EmojiPicker
+              onChange={(emoji) => {
+                if (textAreaRef.current) {
+                  textAreaRef.current.focus();
+                }
+                setMessage(message + emoji);
+              }}
+            />
           </div>
         </motion.div>
         {message.trim() ? (
           <Button
-          className="h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
-          variant={"ghost"}
-          size={"icon"}
+            className="h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
+            variant={"ghost"}
+            size={"icon"}
           >
-            <SendHorizonal size={20} className="text-muted-foreground"
-            onClick={handleSendMessage}
+            <SendHorizonal
+              size={20}
+              className="text-muted-foreground"
+              onClick={handleSendMessage}
             />
           </Button>
         ) : (
           <Button
-          className="h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
-          variant={"ghost"}
-          size={"icon"}
+            className="h-9 w-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
+            variant={"ghost"}
+            size={"icon"}
           >
-            {!isPending && <ThumbsUp size={20} className="text-muted-foreground" 
-            onClick={()=>{
-              sendMessage({content: "👍",messageType: "text", receiverId: selectedUser?.id!, })
-            }}
-             />}
+            {!isPending && (
+              <ThumbsUp
+                size={20}
+                className="text-muted-foreground"
+                onClick={() => {
+                  sendMessage({
+                    content: "👍",
+                    messageType: "text",
+                    receiverId: selectedUser?.id!,
+                  });
+                }}
+              />
+            )}
             {isPending && <Loader size={20} className="animate-spin" />}
           </Button>
         )}
       </AnimatePresence>
-     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default ChatBottomBar
+export default ChatBottomBar;
